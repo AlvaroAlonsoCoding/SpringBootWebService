c459e5d9cb41ba677e5adf005e1ce40d1684cfe3
AlvaroAlonsoCoding <varorocks@gmai.com>
2023-12-01 11:39:16 +0100

    Initial project setup

---
+++ src/test/java/com/varocode/springbootws/SpringbootwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varocode.springbootws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
